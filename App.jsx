70072a95010b56321f63f8b4603009e1e056e8ee
Matt <matt@matt.buzz>
2025-09-15 16:02:58 -0700

    Fix homepage layout: Show only main component
    - Removed all other sections from homepage
    - Homepage now shows only nav + header (main component)
    - Other sections (about, portfolio, etc.) will be separate pages
    - Cleaner single-page layout as intended

--- App.jsx
+++ App.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import './framer/styles.css';
 
-// Import Framer components
+// Import main component (includes nav and header)
 import MainFramerComponent from './framer/main.jsx';
-import NavFramerComponent from './framer/nav.jsx';
-import HeaderFramerComponent from './framer/header.jsx';
-import AboutFramerComponent from './framer/about.jsx';
-import PortfolioFramerComponent from './framer/portfolio.jsx';
-import ExperienceFramerComponent from './framer/experience.jsx';
-import EduFramerComponent from './framer/edu.jsx';
-import FooterFramerComponent from './framer/footer.jsx';
 
 export default function App() {
   return (
     <div className="app-container">
-      {/* Use the main component which already includes nav and header */}
+      {/* Main page with nav and header - other sections will be separate pages */}
       <MainFramerComponent.Responsive />
-
-      {/* Add other sections */}
-      <AboutFramerComponent.Responsive />
-
-      <PortfolioFramerComponent.Responsive />
-
-      <ExperienceFramerComponent.Responsive />
-
-      <EduFramerComponent.Responsive />
-
-      <FooterFramerComponent.Responsive />
     </div>
   );
 }
